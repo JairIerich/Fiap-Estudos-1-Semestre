4a09cd823a9a32d84088ebce577c41a30d0b3bbf
JairIerich <neto.ierich@hotmail.com>
2026-04-07 22:39:13 -0300

    javascript

---
+++ Web Development/script.js
@@ -0,0 +1,10 @@
+alert("Hello Word!!!")
+let nome = prompt("qual seu nome?")
+let idade = prompt("sua idade?")
+let trabalho = prompt ("com oq vc trabalha")
+confirm("voce tem certeza disso?")
+
+console.log("teste!")
+
+alert(`ola ${nome}, voce tem ${idade} anos e trabalha como ${trabalho   }`)
+const pi = 3.14159;
